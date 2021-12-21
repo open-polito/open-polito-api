0baf2e81ea99e627da4ef286d9721d9840450f77
CapacitorSet <capacitorset@gmail.com>
2021-12-21 16:04:54 +0100

    Remove testing server from example

--- example.ts
+++ example.ts
@@ -2,7 +2,7 @@ import { getContexts, getSlots } from "./booking";
 import Device from "./device";
 import { getTimetable } from "./timetable";
 
-const device = new Device("ea27a150-39d5-4f6a-ae1e-51f38bfe0039", () => {}, "http://127.0.0.1:8080/");
+const device = new Device("ea27a150-39d5-4f6a-ae1e-51f38bfe0039", () => {});
 (async () => {
     await device.register();
     const {user, token} = await device.loginWithCredentials("S123456", "password");
