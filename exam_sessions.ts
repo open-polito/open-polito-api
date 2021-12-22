4fff51622ebfa2244eb4669e9dcff1bac4fffd8a
CapacitorSet <capacitorset@gmail.com>
2021-12-22 17:47:57 +0100

    Add booking/cancelling for exam sessions

--- exam_sessions.ts
+++ exam_sessions.ts
@@ -3,6 +3,7 @@ import { checkError } from "./utils";
 import { parse as parseDate } from "date-format-parse";
 
 type ExamSession = {
+    session_id: number
     exam_id: string // 01URPOV
     exam_name: string // 'Machine learning for vision and multimedia (AA-ZZ)'
     date: Date
@@ -17,6 +18,7 @@ export async function getExamSessions(device: Device): Promise<ExamSession[]> {
     checkError(data);
     console.log(data.data.esami.data);
     return data.data.esami.data.map(e => ({
+        session_id: e.ID_VERBALE,
         exam_id: e.COD_INS_STUDENTE,
         exam_name: e.NOME_INS,
         date: parseDate(e.DATA_APPELLO + ' ' + e.ORA_APPELLO, "DD/MM/YYYY hh:mm"),
@@ -25,4 +27,16 @@ export async function getExamSessions(device: Device): Promise<ExamSession[]> {
         error_msg: (e.DESCR_MSG == "CONTROLLO SUPERATO") ? "" : e.DESCR_MSG,
         signup_deadline: parseDate(e.SCADENZA, "DD/MM/YYYY hh:mm"),
     }) as ExamSession)
+}
+
+// Returns an exam session ID that is currently unused.
+export async function bookExamSession(device: Device, session_id: number, exam_id: string): Promise<number> {
+    const data = await device.post("esami.php", { operazione: "PRENOTA", cod_ins: exam_id, id_verbale: session_id });
+    checkError(data);
+    return data.data.esami.id;
+}
+
+export async function cancelExamSession(device: Device, session_id: number, exam_id: string): Promise<number> {
+    const data = await device.post("esami.php", { operazione: "ANNULLA", cod_ins: exam_id, id_verbale: session_id });
+    checkError(data);
 }
