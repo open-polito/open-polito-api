bcb6431ec67da9f934fe53569d30788b110f1e93
CapacitorSet <capacitorset@gmail.com>
2022-02-05 12:52:10 +0100

    Refactor ExamSessions

--- exam_sessions.ts
+++ exam_sessions.ts
@@ -2,22 +2,31 @@ import { Device } from "./device";
 import { checkError } from "./utils";
 import { parse as parseDate } from "date-format-parse";
 
+/** A session on which an exam can be sat */
 export type ExamSession = {
-    session_id: number // A unique ID for this session
-    course_id: string // 01URPOV
-    signup_id: number // Unknown value
-    exam_name: string // 'Machine learning for vision and multimedia (AA-ZZ)'
+    /** A unique ID for this session */
+    session_id: number
+    /** @example "01URPOV" */
+    course_id: string
+    /** Unknown */
+    signup_id: number
+    /** @example "Machine learning for vision and multimedia (AA-ZZ)" */
+    exam_name: string
     user_is_signed_up: boolean
-    date: Date
+    /** The date when this session will take place, as Unix epoch */
+    date: number
     rooms: string[]
-    type: string // 'Scritto e Orale'
+    /** @example "Scritto e Orale" */
+    type: string
     error: {
         id: number
         ita: string
         eng: string
     }
-    error_msg: string // An error message if the user can't sign up for the session. Empty otherwise.
-    signup_deadline: Date
+    /** An error message if the user can't sign up for the session. Empty otherwise */
+    error_msg: string
+    /** The deadline for signing up for this session, as Unix epoch */
+    signup_deadline: number
 };
 
 // Returns exam sessions starting with the closest one.
@@ -30,7 +39,7 @@ export async function getExamSessions(device: Device): Promise<ExamSession[]> {
         signup_id: e.ID,
         exam_name: e.NOME_INS,
         user_is_signed_up: e.ID != -1,
-        date: parseDate(e.DATA_APPELLO + ' ' + e.ORA_APPELLO, "DD/MM/YYYY hh:mm"),
+        date: parseDate(e.DATA_APPELLO + ' ' + e.ORA_APPELLO, "DD/MM/YYYY hh:mm").getTime(),
         rooms: e.AULA.split("; "),
         type: e.DESC_TIPO,
         error: {
@@ -38,7 +47,7 @@ export async function getExamSessions(device: Device): Promise<ExamSession[]> {
             ita: (e.ID_MSG == 0 || e.ID != -1) ? "" : e.DESCR_MSG,
             eng: (e.ID_MSG == 0 || e.ID != -1) ? "" : e.DESCR_MSG_ENG,
         },
-        signup_deadline: parseDate(e.SCADENZA, "DD/MM/YYYY hh:mm"),
+        signup_deadline: parseDate(e.SCADENZA, "DD/MM/YYYY hh:mm").getTime(),
     }) as ExamSession).sort((a, b) => a.date - b.date);
 }
 
